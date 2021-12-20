5b36d774397c4b12b3e111f1fdf42b745aa24dad
robinalva28 <robinnet28@hotmail.com>
2021-12-20 11:38:20 -0300

    CRUD Springboot, JPA, Hibernate, postman

---
+++ src/test/java/com/springrobinson/crudjpahibernatemysql/CrudJpaHibernateMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrobinson.crudjpahibernatemysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJpaHibernateMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/springrobinson/crudjpahibernatemysql/controller/ClienteControllerTest.java
@@ -0,0 +1,24 @@
+package com.springrobinson.crudjpahibernatemysql.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteControllerTest {
+
+    @Test
+    void getClientes() {
+    }
+
+    @Test
+    void crearCliente() {
+    }
+
+    @Test
+    void borrarCliente() {
+    }
+
+    @Test
+    void editarCliente() {
+    }
+}
